15550638f6db036f2336bf09a59d187406428eaf
Spatulox <M78stormtrooper@laposte.net>
2024-12-03 22:03:13 +0100

    add json.go
    add ReadJsonFile

--- src/json/json.go
+++ src/json/json.go
@@ -1,1 +1,22 @@
 package json
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func ReadJsonFile[T any](filePath string, structure *T) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(structure)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
