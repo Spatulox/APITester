0d71ab236980535828ab6ff1c7c89a083329cb20
Spatulox <M78stormtrooper@laposte.net>
2024-12-03 21:15:11 +0100

    add struct.go to match the config.json

--- struct.go
+++ struct.go
@@ -1,1 +1,37 @@
 package main
+
+type EndPoint struct {
+	BasicURL       string         `json:"basicUrl"`
+	Authentication Authentication `json:"authentication"`
+}
+
+type Authentication struct {
+	Type      string     `json:"type"`
+	APIKey    string     `json:"apikey"`
+	OAuth2    OAuth2     `json:"oauth2"`
+	BasicAuth BasicAuth  `json:"basicAuth"`
+	Endpoint  []Endpoint `json:"endpoints"`
+}
+
+type OAuth2 struct {
+	ClientID     string `json:"clientId"`
+	ClientSecret string `json:"clientSecret"`
+	TokenURL     string `json:"tokenUrl"`
+}
+
+type BasicAuth struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Endpoint struct {
+	Path  string `json:"path"`
+	Tests []Test `json:"tests"`
+}
+
+type Test struct {
+	Method            string                 `json:"method"`
+	Input             map[string]interface{} `json:"input,omitempty"` // Optional field
+	ExpectedOutput    map[string]interface{} `json:"expectedOutput"`
+	ExpectedHttpState string                 `json:"expectedHttpState"`
+}
