8382f55fb553b15f1a6681ffe6c4afebd4172c1f
Spatulox <M78stormtrooper@laposte.net>
2024-12-05 13:31:43 +0100

    update json pkg | - add ListJsonFile |- add SaveConfigToJson

--- src/json/json.go
+++ src/json/json.go
@@ -1,8 +1,12 @@
 package json
 
 import (
+	. "ApiTester/src/log"
+	. "ApiTester/src/struct"
 	"encoding/json"
+	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func ReadJsonFile[T any](filePath string, structure *T) error {
@@ -18,5 +22,84 @@ func ReadJsonFile[T any](filePath string, structure *T) error {
 		return err
 	}
 
+	return nil
+}
+
+func ListJsonFile() (map[string]interface{}, error) {
+	appDataPath, err := os.UserConfigDir()
+	if err != nil {
+		return nil, fmt.Errorf("error obtaining AppData/conf folder: %v", err)
+	}
+
+	// Create a map to store files by folder
+	folderFiles := make(map[string]interface{})
+
+	pathDir := filepath.Join(appDataPath, "ApiTester")
+
+	err = filepath.Walk(pathDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil // Ignore directories
+		}
+
+		// Extract the name of the parent directory
+		parentDir := filepath.Dir(path)
+		folderName := filepath.Base(parentDir)
+
+		if folderName == "ApiTester" {
+			folderName = "root" // Use "root" for files at the root level of ApiTester
+		}
+
+		// Check if the folder already exists in the map
+		if _, exists := folderFiles[folderName]; !exists {
+			folderFiles[folderName] = []string{} // Initialize as a slice of strings
+		}
+
+		// Add the file to the folder's list
+		folderFiles[folderName] = append(folderFiles[folderName].([]string), info.Name())
+		return nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error reading files: %v", err)
+	}
+
+	return folderFiles, nil
+}
+
+func SaveConfigToJson(config Config, path string, filename string) error {
+	appDataPath, err := os.UserConfigDir()
+	if err != nil {
+		return fmt.Errorf("error obtaining AppData folder: %v", err)
+	}
+
+	if filename == "" {
+		return fmt.Errorf("error: filename is empty")
+	}
+
+	pathDir := filepath.Join(appDataPath, "ApiTester")
+	pathDir = filepath.Join(pathDir, path)
+
+	if err := os.MkdirAll(pathDir, os.ModePerm); err != nil {
+		return fmt.Errorf("error creating directory %s: %v", pathDir, err)
+	}
+
+	filePath := filepath.Join(pathDir, filename)
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("could not create file: %v", err)
+	}
+	defer file.Close()
+
+	// JSON Encoder
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ") // Human-readable format
+	if err := encoder.Encode(config); err != nil {
+		return fmt.Errorf("could not encode config to JSON: %v", err)
+	}
+	Log.Infos(fmt.Sprintf("Save successful in %s", pathDir))
 	return nil
 }
