1a85182ffbc92aebb40e9da23bb7e04e328e543c
Spatulox <M78stormtrooper@laposte.net>
2024-12-06 16:28:08 +0100

    update main.js
    list of file are now have an automatic refresh

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,16 @@
 import { createFileList } from "./JS/create-DOM-element"
 import {ListJsonFile} from "../wailsjs/go/main/App";
+import { OpenFileExplorer } from "../wailsjs/go/main/App";
 
+
+document.getElementById("viewTestFilesButton").onclick = async () => {
+    try {
+        await OpenFileExplorer();
+    } catch (error) {
+        console.error("Erreur lors de l'ouverture de l'explorateur de fichiers:", error);
+        alert("Erreur: " + error.toString());
+    }
+};
 
 
 async function getSortedFolderFiles() {
@@ -27,9 +37,19 @@ async function getSortedFolderFiles() {
     return sortedFolderFiles;
 }
 
+let oldListFolder = []
+export async function refreshFile() {
+    window.folderFiles = await getSortedFolderFiles();
+
+    if (JSON.stringify(window.folderFiles) !== JSON.stringify(oldListFolder)) {
+        oldListFolder = JSON.parse(JSON.stringify(window.folderFiles));
+        createFileList(window.folderFiles);
+    }
+}
+
 async function startup(){
-    window.folderFiles = await getSortedFolderFiles()
-    createFileList(window.folderFiles);
+    refreshFile()
+    setInterval(() => { refreshFile() }, 3000);
 }
 
 startup()
