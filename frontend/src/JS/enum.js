ad01ff30b3bd6d4992633d1eeaff6c55750721b2
Spatulox <M78stormtrooper@laposte.net>
2024-12-11 18:14:22 +0100

    add it to the front

--- frontend/src/JS/enum.js
+++ frontend/src/JS/enum.js
@@ -14,7 +14,7 @@ const ResultError = {
     11: 'ErrorNetwork',
     12: 'ErrorHTTP',
     13: 'ErrorConnectionRefused',
-    14: 'ErrorExpectedOrActualOuputLenght',
+    14: 'ErrorExpectedOrActualOuputNotMatch',
     400: 'ErrorBadRequest',
     401: 'ErrorUnauthorized',
     402: 'ErrorPayementRequired',
